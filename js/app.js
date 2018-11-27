1235e32185c8ff388340d76706a31cf496c692d4
Clara Munro <clarajmunro@gmail.com>
2018-11-27 15:12:04 -0800

    Added 5 questions & some CSS

---
+++ js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var startGame = confirm('Do you want to learn all ABOUT ME?!');
+
+if(startGame) {
+  alert('Great, let\'s play!');
+} else {
+  alert('Ok see you later :(');
+}
+
+var favColor = prompt('Do you think my favorite color is yellow? ').toUpperCase();
+
+if(favColor === 'YES') {
+    alert('You are correct!! I like Yellow.');
+  } else if (favColor === 'NO') {
+    alert('WRONG! I like yellow.');
+  } else {
+    alert('Please answer with yes or no! Use words people.');
+  }
+
+var favFood = prompt('Do you think my favorite food is sushi?').toUpperCase();
+
+if(favFood === 'YES') {
+  alert('You are correct!! I like sushi.');
+} else if (favFood === 'NO') {
+  alert('WRONG! I like sushi.');
+} else {
+  alert('Please answer with yes or no! Use words people.');
+}
+
+var bananas = prompt('Do you think I like bananas?').toUpperCase();
+
+if(bananas === 'YES') {
+  alert('You are correct!! I like bananas.');
+} else if (bananas === 'NO') {
+  alert('WRONG! I like bananas.');
+} else {
+  alert('Please answer with yes or no! Use words people.');
+}
+
+var coffee = prompt('Do you think I like coffee?').toUpperCase();
+
+if(coffee === 'YES') {
+  alert('You are correct!! I like coffee.');
+} else if (coffee === 'NO') {
+  alert('WRONG! I like coffee.');
+} else {
+  alert('Please answer with yes or no! Use words people.');
+}
+
+var basketball = prompt('Do you think I like basketball?').toUpperCase();
+
+if(basketball === 'YES') {
+  alert('You are correct!! I like basketball.');
+} else if (basketball === 'NO') {
+  alert('WRONG! I like basketball.');
+} else {
+  alert('Please answer with yes or no! Use words people.');
+}
+
