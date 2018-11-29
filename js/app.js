9407f79e18227c8ef63185f8c5c47ec29815682e
Clara Munro <clarajmunro@gmail.com>
2018-11-29 14:36:34 -0800

    Fixed question 6

--- js/app.js
+++ js/app.js
@@ -1,117 +1,133 @@
-'use strict';
+// 'use strict';
 
-var startGame = confirm('Do you want to learn all ABOUT ME?!');
+var counterCorrect = 0;
+// var startGame = confirm('Do you want to learn all ABOUT ME?!');
 
-if(startGame) {
-  alert('Great, let\'s play!');
-  console.log('They want to play a game.');
-} else {
-  alert('Ok see you later :(');
-  console.log('They want don\'t want to play a game.');
-}
+// if(startGame) {
+//   alert('Great, let\'s play!');
+//   console.log('They want to play a game.');
+// } else {
+//   alert('Ok see you later :(');
+//   console.log('They want don\'t want to play a game.');
+// }
 
-var name = prompt('What is your name?');
+// var name = prompt('What is your name?');
 
-// Question 1
-var favColor = prompt('Do you think my favorite color is yellow?').toUpperCase();
+// // Question 1
+// var favColor = prompt('Do you think my favorite color is yellow?').toUpperCase();
 
-if(favColor === 'YES') {
-  alert('You are correct!! I like Yellow.');
-  console.log('Correct about Yellow.');
-} else if (favColor === 'NO') {
-  alert('WRONG! I like yellow.');
-  console.log('Wrong about Yellow.');
-} else {
-  alert('Please answer with yes or no! Use words people.');
-  console.log('Didn\'t use yes or no, on Yellow Question.');
-}
+// if(favColor === 'YES') {
+//   alert('You are correct!! I like Yellow.');
+//   console.log('Correct about Yellow.');
+//   counterCorrect++
+//   console.log('yellow: ' + counterCorrect);
+// } else if (favColor === 'NO') {
+//   alert('WRONG! I like yellow.');
+//   console.log('Wrong about Yellow.');
+// } else {
+//   alert('Please answer with yes or no! Use words people.');
+//   console.log('Didn\'t use yes or no, on Yellow Question.');
+// }
 
-// Question 2
-var favFood = prompt('Do you think my favorite food is sushi?').toUpperCase();
+// // Question 2
+// var favFood = prompt('Do you think my favorite food is sushi?').toUpperCase();
 
-if(favFood === 'YES') {
-  alert('You are correct!! I like sushi.');
-  console.log('Right about Sushi.');
-} else if (favFood === 'NO') {
-  alert('WRONG! I like sushi.');
-  console.log('Wrong about Sushi.');
-} else {
-  alert('Please answer with yes or no! Use words people.');
-  console.log('Didn\'t use yes or no, on Sushi Question.');
+// if(favFood === 'YES') {
+//   alert('You are correct!! I like sushi.');
+//   console.log('Right about Sushi.');
+//   counterCorrect++
+//   console.log('sushi: ' + counterCorrect);
+// } else if (favFood === 'NO') {
+//   alert('WRONG! I like sushi.');
+//   console.log('Wrong about Sushi.');
+// } else {
+//   alert('Please answer with yes or no! Use words people.');
+//   console.log('Didn\'t use yes or no, on Sushi Question.');
 
-}
+// }
 
-// Question 3
-var bananas = prompt('Do you think I like bananas?').toUpperCase();
+// // Question 3
+// var bananas = prompt('Do you think I like bananas?').toUpperCase();
 
-if(bananas === 'YES') {
-  alert('You are correct!! I like bananas.');
-  console.log('Right about bananas.');
-} else if (bananas === 'NO') {
-  alert('WRONG! I like bananas.');
-  console.log('Wrong about bananas.');
-} else {
-  alert('Please answer with yes or no! Use words people.');
-  console.log('Didn\'t use yes or no, on Bananas Question.'); 
+// if(bananas === 'YES') {
+//   alert('You are correct!! I like bananas.');
+//   console.log('Right about bananas.');
+//   counterCorrect++
+//   console.log('bananas: ' + counterCorrect);
+// } else if (bananas === 'NO') {
+//   alert('WRONG! I like bananas.');
+//   console.log('Wrong about bananas.');
+// } else {
+//   alert('Please answer with yes or no! Use words people.');
+//   console.log('Didn\'t use yes or no, on Bananas Question.');
 
-}
+// }
 
-// Question 4
-var coffee = prompt('Do you think I like coffee?').toUpperCase();
+// // Question 4
+// var coffee = prompt('Do you think I like coffee?').toUpperCase();
 
-if(coffee === 'YES') {
-  alert('You are correct!! I like coffee.');
-  console.log('Right about coffee.');
-} else if (coffee === 'NO') {
-  alert('WRONG! I like coffee.');
-  console.log('Wrong about coffee.');
-} else {
-  alert('Please answer with yes or no! Use words people.');
-  console.log('Didn\'t use yes or no, on Coffee Question.');
-}
+// if(coffee === 'YES') {
+//   alert('You are correct!! I like coffee.');
+//   console.log('Right about coffee.');
+//   counterCorrect++
+//   console.log('coffee: ' + counterCorrect);
+// } else if (coffee === 'NO') {
+//   alert('WRONG! I like coffee.');
+//   console.log('Wrong about coffee.');
+// } else {
+//   alert('Please answer with yes or no! Use words people.');
+//   console.log('Didn\'t use yes or no, on Coffee Question.');
+// }
 
-// Question 5
-var basketball = prompt('Do you think I like basketball?').toUpperCase();
+// // Question 5
+// var basketball = prompt('Do you think I like basketball?').toUpperCase();
 
-if(basketball === 'YES') {
-  alert('You are correct!! I like basketball.');
-  console.log('Right I like basketball');
-} else if (basketball === 'NO') {
-  alert('WRONG! I like basketball.');
-  console.log('Wrong like basketball');
-} else {
-  alert('Please answer with yes or no! Use words people.');
-  console.log('Didn\'t use yes or no, on Basketball Question.');
-}
-
-
-// Question 6
-
-alert('Can you guess my favorite number?');
-
-var counter = 0;
-
-while (counter < 5) {
-  var answer = prompt('What’s your guess?');
-
-  if ( answer === 3) {
-  alert ('Right answer');
-  ++counterCorrect
-  break;
-  }
-
-  else if (answer > 3){
-    alert('your number is too high');
-    ++counter
-  }
-  
-  else if (answer < 3){
-   alert('your number is too low');
-   ++counter
- }
+// if(basketball === 'YES') {
+//   alert('You are correct!! I like basketball.');
+//   console.log('Right I like basketball');
+//   counterCorrect++
+//   console.log('basketball: ' + counterCorrect);
+// } else if (basketball === 'NO') {
+//   alert('WRONG! I like basketball.');
+//   console.log('Wrong like basketball');
+// } else {
+//   alert('Please answer with yes or no! Use words people.');
+//   console.log('Didn\'t use yes or no, on Basketball Question.');
+// }
 
 
-alert('Oh man, 4 tries is up, the answer is was 3!');
+// // Question 6
+
+// alert('Can you guess my favorite number?');
+// var counter = 0;
+
+// while (counter < 5) {
+//   var answer = prompt('What\'s your guess?');
+//   answer = parseInt(answer);
+
+//   if (answer === 3) {
+//     alert ('Right answer');
+//     counterCorrect++;
+//     console.log('favorite number' + counterCorrect);
+//     break;
+//   }
+
+//   else if (answer > 3) {
+//     alert ('your number is too high');
+//     counter++;
+//     console.log(counter);
+//   }
+
+//   else if (answer < 3){
+//     alert('your number is too low');
+//     counter++;
+//     console.log(counter);
+//   }
+
+//   if (counter === 5) {
+//     alert('Oh man, 4 tries is up, the answer is was 3!');
+//   }
+// }
 
 
 
@@ -119,32 +135,27 @@ alert('Oh man, 4 tries is up, the answer is was 3!');
 
 alert('Can you guess the states I\'ve lived in?');
 
-var states = ['Oregon', 'Washington', 'Missouri']
-states = states.toUpperCase();
+var states = ['oregon', 'washington', 'missouri', 'kansas'];
+var counterB = 0;
 
-var answer = prompt('What is your guess?');
-answer = answer.toUpperCase();
+do {
+  var answerB = prompt('What is your guess?');
+  answerB = answerB.toLowerCase();
 
-for ( i=1 ; i<7 ; i++ ); {
-
-  if (answer === states[i]) {
-    alert ('you got it right');
-    ++counterCorrect
-    break;
-    }
-
-  else {
-    alert('wrong answer');
-    i++
+  if (states.includes(answerB)) {
+    alert ('You got it right');
+    counterCorrect++;
+    console.log('states' + counterCorrect);
+    // break;
   }
-}
 
-  alert('6 tries is up, the answer is ...');
+  else if (answerB === !states.includes(answerB)) {
+    alert('wrong answer');
+    counterB++;
+    }
+  } while (counterB < 4 )
 
-  alert('You got ' + counterCorrect + ' out of 7 questions,' + name );
-  
-}
 
-// if (cityWouldLive.includes(cityGuessed)) {
-//   ... 
+// if (counterB === 4) {
+//   alert('5 tries is up, the answer is ...');
 // }
